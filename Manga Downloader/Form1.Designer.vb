1db3613ff288a037fe117aecfc3cde6d444bbb57
codingeass <amandeeptheviper@gmail.com>
2015-04-11 17:11:00 +0530

    Added all files of Managa Downloader

---
+++ Manga Downloader/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(280, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Manga Downloader"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(53, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(79, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Select Website"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(476, 54)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 2
+        Me.ComboBox1.Text = "Kissmanga.com"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(53, 116)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 13)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Enter Url"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(343, 113)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(289, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(38, 259)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(452, 108)
+        Me.ListBox1.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(244, 158)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(159, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(550, 284)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(169, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Download Selected"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(748, 379)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Managa Downloader"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Manga Downloader/Form1.vb
@@ -0,0 +1,127 @@
+﻿Imports System
+Imports System.IO
+Imports System.Net
+Imports System.Text.RegularExpressions
+Public Class Form1
+    Dim enteredUrl As String
+    Dim DownloadUrl() As String
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        enteredUrl = TextBox1.Text
+        'Dim sourceString As String = New System.Net.WebClient().DownloadString(enteredUrl)
+        'MsgBox(sourceString)
+        'WebBrowser1.Navigate(enteredUrl)
+        'Dim htmlSourceCode As String = WebBrowser1.DocumentText.ToString()
+        'Dim bodyContent As String = WebBrowser1.Document.Body.InnerText
+        Dim request As HttpWebRequest = WebRequest.Create(enteredUrl)
+        request.UserAgent = ".NET Framework Test Client"
+        Dim response As HttpWebResponse = request.GetResponse()
+        Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
+        'Dim str As String = reader.ReadLine()
+        Dim str As String = reader.ReadToEnd
+        'RichTextBox1.Text = str
+        'Dim myRegExp As RegExp
+        'Dim myMatches As MatchCollection
+        'Dim myMatch As Match
+        'myRegExp = New RegExp
+        'myRegExp.IgnoreCase = True
+        'myRegExp.Global = True
+        'myRegExp.Pattern = "regex"
+        'myMatches = myRegExp.Execute(subjectString)
+        'For Each myMatch In myMatches
+        '    MsgBox(myMatch.Value)
+        'Next
+        Dim regex As Regex = New Regex("<table class=""listing"">.*?</table>")
+        Dim regexStr As String = "<table class=""listing"">.*?<\/table>"
+        Dim match As Match = regex.Match(str, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+        If match.Success Then
+            'RichTextBox1.Text = match.Value
+        Else
+            MsgBox("Error in input" & match.Value)
+        End If
+        regex = New Regex("<a href="".*?</a>")
+        regexStr = "<a href="".*?</a>"
+        Dim matches As MatchCollection = regex.Matches(match.Value, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+        Dim i As Integer = 0
+        Dim StrMatch As String = ""
+        Dim MangaChapter() As String
+        If matches.Count <> 0 Then
+            ReDim MangaChapter(matches.Count)
+            ReDim DownloadUrl(matches.Count)
+        End If
+        ListBox1.Items.Clear()
+        While i < matches.Count
+            MangaChapter(i) = matches(i).Value.Replace("<a href=", "")
+            MangaChapter(i) = MangaChapter(i).Replace("""", "")
+            regex = New Regex(">.*?</a>")
+            regexStr = ">.*?</a>"
+            match = regex.Match(MangaChapter(i), regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+            StrMatch += MangaChapter(i)
+            'ListBox1.Items.Add(MangaChapter(i).Split.First)
+            DownloadUrl(i) = MangaChapter(i).Split.First
+            ListBox1.Items.Add(match.Value.Replace("</a>", "").Replace(">", ""))
+            i += 1
+        End While
+        'MsgBox(StrMatch)
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim URL As String = "http://kissmanga.com/Manga/Haru-Koi"
+        Dim request As HttpWebRequest = WebRequest.Create(URL)
+        request.UserAgent = ".NET Framework Test Client"
+        Dim response As HttpWebResponse = request.GetResponse()
+        Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
+        'Dim str As String = reader.ReadLine()
+        Dim str As String = reader.ReadToEnd
+        'Do While str.Length > 0
+        '    Console.WriteLine(str)
+        '    str += reader.ReadLine()
+        'Loop
+        'MsgBox(complete)
+        'KissMangaImage("/Manga/Haru-Koi/Vol-001-Ch-004--Misa-s-Eatery?id=115540")
+    End Sub
+
+    Private Sub KissMangaImage(url As String)
+        Dim KissMangaUrlCom As String = "http://kissmanga.com" + url
+        Dim request As HttpWebRequest = WebRequest.Create(KissMangaUrlCom)
+        request.UserAgent = ".NET Framework Test Client"
+        Dim response As HttpWebResponse = request.GetResponse()
+        Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
+        Dim str As String = reader.ReadToEnd
+
+        Dim regex As Regex = New Regex("lstImages.push.*?""\);")
+        Dim regexStr As String = "lstImages.push.*?""\);"
+        Dim match As MatchCollection = regex.Matches(str, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+        Dim i As Integer = 0
+        Dim Client As New WebClient
+        'MsgBox(match.Count)
+        Dim Destination As String
+        Dim dialog As New FolderBrowserDialog()
+        dialog.RootFolder = Environment.SpecialFolder.Desktop
+        dialog.SelectedPath = "C:\"
+        dialog.Description = "Select Path to Download Manga"
+        If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+            Destination = dialog.SelectedPath
+        End If
+
+        While i < match.Count
+            Try
+                Client.DownloadFile(match(i).Value.Replace("lstImages.push(""", "").Replace(""");", ""), Destination & i.ToString & ".jpg")
+                Client.Dispose()
+            Catch e As Exception
+                MsgBox(e.Message)
+            End Try
+            'RichTextBox1.AppendText(match(i).Value.Replace("lstImages.push(""", "").Replace(""");", ""))
+            i += 1
+        End While
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        KissMangaImage(DownloadUrl(ListBox1.SelectedIndex))
+    End Sub
+
+End Class
