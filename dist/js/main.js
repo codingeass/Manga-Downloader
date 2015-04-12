cc03cdf6f7ffe0c2806958dd98652d23cfb9675c
codingeass <amandeeptheviper@gmail.com>
2015-04-12 20:32:02 +0530

    added main page and details of app

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+
+function removeCarousel()
+{
+	
+	if(screen.availWidth<996)
+	{
+		//alert(document.getElementById('projects').innerHTML);
+		//var proj=document.getElementById('content_dir').getElementById('projects');
+		document.getElementById('slider_remove_id').className="";	
+		document.getElementById('arrow').innerHTML="";
+	}
+}
