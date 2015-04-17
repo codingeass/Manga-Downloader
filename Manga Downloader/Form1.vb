ea383d86e2b6100e77c5345ae0300ddac67bd06f
codingeass <amandeeptheviper@gmail.com>
2015-04-17 19:37:48 +0530

    added exception message

--- Manga Downloader/Form1.vb
+++ Manga Downloader/Form1.vb
@@ -6,67 +6,70 @@ Public Class Form1
     Dim enteredUrl As String
     Dim DownloadUrl() As String
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        enteredUrl = TextBox1.Text
-        'enteredUrl = "http://kissmanga.com/Manga/Hatsukoi"
-        'Dim sourceString As String = New System.Net.WebClient().DownloadString(enteredUrl)
-        'MsgBox(sourceString)
-        'WebBrowser1.Navigate(enteredUrl)
-        'Dim htmlSourceCode As String = WebBrowser1.DocumentText.ToString()
-        'Dim bodyContent As String = WebBrowser1.Document.Body.InnerText
-        Dim request As HttpWebRequest = WebRequest.Create(enteredUrl)
-        request.UserAgent = ".NET Framework Test Client"
-        Dim response As HttpWebResponse = request.GetResponse()
-        Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
-        'Dim str As String = reader.ReadLine()
-        Dim str As String = reader.ReadToEnd
-        'RichTextBox1.Text = str
-        'Dim myRegExp As RegExp
-        'Dim myMatches As MatchCollection
-        'Dim myMatch As Match
-        'myRegExp = New RegExp
-        'myRegExp.IgnoreCase = True
-        'myRegExp.Global = True
-        'myRegExp.Pattern = "regex"
-        'myMatches = myRegExp.Execute(subjectString)
-        'For Each myMatch In myMatches
-        '    MsgBox(myMatch.Value)
-        'Next
-        Dim regex As Regex = New Regex("<table class=""listing"">.*?</table>")
-        Dim regexStr As String = "<table class=""listing"">.*?<\/table>"
-        Dim match As Match = regex.Match(str, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
-        If match.Success Then
-            'RichTextBox1.Text = match.Value
-        Else
-            MsgBox("Error in input" & match.Value)
-        End If
-        regex = New Regex("<a href="".*?</a>")
-        regexStr = "<a href="".*?</a>"
-        Dim matches As MatchCollection = regex.Matches(match.Value, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
-        Dim i As Integer = 0
-        Dim StrMatch As String = ""
-        Dim MangaChapter() As String
-        Dim DisplayChapter As String
-        If matches.Count <> 0 Then
-            ReDim MangaChapter(matches.Count)
-            ReDim DownloadUrl(matches.Count)
-        End If
-        CheckedListBox1.Items.Clear()
-        While i < matches.Count
-            MangaChapter(i) = matches(i).Value.Replace("<a href=", "")
-            MangaChapter(i) = MangaChapter(i).Replace("""", "")
-            regex = New Regex(">.*?</a>")
-            regexStr = ">.*?</a>"
-            match = regex.Match(MangaChapter(i), regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
-            StrMatch += MangaChapter(i)
-            'ListBox1.Items.Add(MangaChapter(i).Split.First)
-            DownloadUrl(i) = MangaChapter(i).Split.First
-            DisplayChapter = match.Value.Replace("</a>", "").Replace(">", "")
-            'ListBox1.Items.Add(DisplayChapter)
-            CheckedListBox1.Items.Add(DisplayChapter.Substring(1))
-            i += 1
-        End While
-        'MsgBox(StrMatch)
-
+        Try
+            enteredUrl = TextBox1.Text
+            'enteredUrl = "http://kissmanga.com/Manga/Hatsukoi"
+            'Dim sourceString As String = New System.Net.WebClient().DownloadString(enteredUrl)
+            'MsgBox(sourceString)
+            'WebBrowser1.Navigate(enteredUrl)
+            'Dim htmlSourceCode As String = WebBrowser1.DocumentText.ToString()
+            'Dim bodyContent As String = WebBrowser1.Document.Body.InnerText
+            Dim request As HttpWebRequest = WebRequest.Create(enteredUrl)
+            request.UserAgent = ".NET Framework Test Client"
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim reader As StreamReader = New StreamReader(response.GetResponseStream())
+            'Dim str As String = reader.ReadLine()
+            Dim str As String = reader.ReadToEnd
+            'RichTextBox1.Text = str
+            'Dim myRegExp As RegExp
+            'Dim myMatches As MatchCollection
+            'Dim myMatch As Match
+            'myRegExp = New RegExp
+            'myRegExp.IgnoreCase = True
+            'myRegExp.Global = True
+            'myRegExp.Pattern = "regex"
+            'myMatches = myRegExp.Execute(subjectString)
+            'For Each myMatch In myMatches
+            '    MsgBox(myMatch.Value)
+            'Next
+            Dim regex As Regex = New Regex("<table class=""listing"">.*?</table>")
+            Dim regexStr As String = "<table class=""listing"">.*?<\/table>"
+            Dim match As Match = regex.Match(str, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+            If match.Success Then
+                'RichTextBox1.Text = match.Value
+            Else
+                MsgBox("Error in input" & match.Value)
+            End If
+            regex = New Regex("<a href="".*?</a>")
+            regexStr = "<a href="".*?</a>"
+            Dim matches As MatchCollection = regex.Matches(match.Value, regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+            Dim i As Integer = 0
+            Dim StrMatch As String = ""
+            Dim MangaChapter() As String
+            Dim DisplayChapter As String
+            If matches.Count <> 0 Then
+                ReDim MangaChapter(matches.Count)
+                ReDim DownloadUrl(matches.Count)
+            End If
+            CheckedListBox1.Items.Clear()
+            While i < matches.Count
+                MangaChapter(i) = matches(i).Value.Replace("<a href=", "")
+                MangaChapter(i) = MangaChapter(i).Replace("""", "")
+                regex = New Regex(">.*?</a>")
+                regexStr = ">.*?</a>"
+                match = regex.Match(MangaChapter(i), regexStr, RegexOptions.IgnoreCase Or RegexOptions.Singleline)
+                StrMatch += MangaChapter(i)
+                'ListBox1.Items.Add(MangaChapter(i).Split.First)
+                DownloadUrl(i) = MangaChapter(i).Split.First
+                DisplayChapter = match.Value.Replace("</a>", "").Replace(">", "")
+                'ListBox1.Items.Add(DisplayChapter)
+                CheckedListBox1.Items.Add(DisplayChapter.Substring(1))
+                i += 1
+            End While
+            'MsgBox(StrMatch)
+        Catch er As Exception
+            MsgBox("Error Message:" & er.Message & vbLf & "Or you are not connected to internet")
+        End Try
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -128,25 +131,28 @@ Public Class Form1
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        Dim i As Integer = 0
-        Dim itemChecked As Object
+        Try
+            Dim i As Integer = 0
+            Dim itemChecked As Object
 
-        Dim Destination As String
-        Dim dialog As New FolderBrowserDialog()
-        dialog.RootFolder = Environment.SpecialFolder.Desktop
-        dialog.SelectedPath = "C:\"
-        dialog.Description = "Select Path to Download Manga"
-        If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
-            Destination = dialog.SelectedPath
-        End If
+            Dim Destination As String
+            Dim dialog As New FolderBrowserDialog()
+            dialog.RootFolder = Environment.SpecialFolder.Desktop
+            dialog.SelectedPath = "C:\"
+            dialog.Description = "Select Path to Download Manga"
+            If dialog.ShowDialog() = Windows.Forms.DialogResult.OK Then
+                Destination = dialog.SelectedPath
+            End If
 
-        For Each itemChecked In CheckedListBox1.CheckedItems
-            'MsgBox(CheckedListBox1.Items.IndexOf(itemChecked))
-            'MsgBox(CheckedListBox1.Items(CheckedListBox1.Items.IndexOf(itemChecked)))
-            'MsgBox(DownloadUrl(CheckedListBox1.Items.IndexOf(itemChecked)))
-            KissMangaImage(DownloadUrl(CheckedListBox1.Items.IndexOf(itemChecked)), CheckedListBox1.Items(CheckedListBox1.Items.IndexOf(itemChecked)), Destination)
-        Next
-
+            For Each itemChecked In CheckedListBox1.CheckedItems
+                'MsgBox(CheckedListBox1.Items.IndexOf(itemChecked))
+                'MsgBox(CheckedListBox1.Items(CheckedListBox1.Items.IndexOf(itemChecked)))
+                'MsgBox(DownloadUrl(CheckedListBox1.Items.IndexOf(itemChecked)))
+                KissMangaImage(DownloadUrl(CheckedListBox1.Items.IndexOf(itemChecked)), CheckedListBox1.Items(CheckedListBox1.Items.IndexOf(itemChecked)), Destination)
+            Next
+        Catch er As Exception
+            MsgBox("Error Message :" & er.Message)
+        End Try
     End Sub
 
 End Class
